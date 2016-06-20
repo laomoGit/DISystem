9f188261f7c256c416a7302fe4b0d38645e90bc1
laomoGit <2510094452@qq.com>
2016-06-20 11:36:10 +0800

    登录界面

---
+++ app/src/main/java/com/mqt/dripirrigationsystem/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.mqt.dripirrigationsystem;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/6/20.
+ */
+public class LoginActivity extends AppCompatActivity{
+
+}
